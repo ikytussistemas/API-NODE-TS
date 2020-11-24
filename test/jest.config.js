0a8260a360c4cac93a4f618c9d0c473a0fd641c3
IkytusSistemas <ikytus.sistemas@gmail.com>
2020-11-23 21:22:41 -0300

    correct test

--- test/jest.config.js
+++ test/jest.config.js
@@ -10,4 +10,5 @@ module.exports = {...rootConfig, ...{
   displayName: "end2end-tests",
   setupFilesAfterEnv: ["<rootDir>/test/jest-setup.ts"],
   testMatch: ["<rootDir>/test/**/*.test.ts"],
+  testTimeout: 500
 }}
